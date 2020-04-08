172376466ce7d64fe575b001be5e5a8affdeac2c
Ángel Barrera Sánchez <angel@sighup.io>
2020-04-08 08:44:29 +0200

    Update ingress core module

--- katalog/tests/ingress.sh
+++ katalog/tests/ingress.sh
@@ -12,6 +12,16 @@ load ./helper
     [ "$status" -eq 0 ]
 }
 
+@test "Cert-manager webhook is Running" {
+    info
+    test() {
+        kubectl get pods -l app=webhook -o json -n cert-manager |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 30 2
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
 @test "Nginx Ingress Controller is Running" {
     info
     test() {
