541ecde2718bf1e356728c9d5275109c7776b4c5
Ángel Barrera Sánchez <angel@sighup.io>
2020-09-21 12:44:41 +0200

    Add new tests

--- katalog/tests/opa.sh
+++ katalog/tests/opa.sh
@@ -12,3 +12,12 @@ load ./helper
     status=${loop_it_result}
     [ "$status" -eq 0 ]
 }
+@test "Gatekeeper Audit is Running" {
+  info
+  test(){
+    kubectl get pods -l control-plane=audit-controller -o json -n gatekeeper-system | jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+  }
+  loop_it test 30 2
+  status=${loop_it_result}
+  [[ "$status" -eq 0 ]]
+}
