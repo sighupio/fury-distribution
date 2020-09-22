14fa6e0c702fb74e0db91d82752b884f302cadbb
Ángel Barrera Sánchez <angel@sighup.io>
2020-09-22 16:33:40 +0200

    Add license headers to the repository

--- katalog/tests/install.sh
+++ katalog/tests/install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bats
+# Copyright (c) 2020 SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 # shellcheck disable=SC2154
 
 load ./helper
