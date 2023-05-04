fe704b628eed1ee44f478e123ad59cd9be83abbf
Alessio Dionisi <me@alessiodionisi.com>
2023-05-04 14:01:51 +0200

    feat: add checksummer tool and checksums of current tools

---
+++ tools/checksummer/go.mod
@@ -0,0 +1,18 @@
+module github.com/sighupio/fury-distribution/tools/checksummer
+
+go 1.20
+
+require (
+	github.com/cheggaaa/pb/v3 v3.1.2
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
---
+++ tools/checksummer/main.go
@@ -0,0 +1,124 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/cheggaaa/pb/v3"
+	"gopkg.in/yaml.v3"
+)
+
+type KFDFile struct {
+	Tools struct {
+		Common map[string]struct {
+			Version   string
+			Checksums map[string]string
+		}
+	}
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Printf("error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	data, err := os.ReadFile("kfd.yaml")
+	if err != nil {
+		return err
+	}
+
+	var kfdFile KFDFile
+	if err := yaml.Unmarshal(data, &kfdFile); err != nil {
+		return err
+	}
+
+	for toolName, tool := range kfdFile.Tools.Common {
+		for checksumOSAndArch := range tool.Checksums {
+			checksumOS, checksumArch, _ := strings.Cut(checksumOSAndArch, "/")
+
+			fmt.Printf("Downloading %s version %s for %s %s...\n", toolName, tool.Version, checksumOS, checksumArch)
+
+			var url string
+			switch toolName {
+			case "furyagent":
+				url = fmt.Sprintf("https://github.com/sighupio/furyagent/releases/download/v%s/furyagent-%s-%s", tool.Version, checksumOS, checksumArch)
+			case "kubectl":
+				url = fmt.Sprintf("https://dl.k8s.io/release/v%s/bin/%s/%s/kubectl", tool.Version, checksumOS, checksumArch)
+			case "terraform":
+				url = fmt.Sprintf("https://releases.hashicorp.com/terraform/%s/terraform_%s_%s_%s.zip", tool.Version, tool.Version, checksumOS, checksumArch)
+			case "kustomize":
+				url = fmt.Sprintf("https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v%s/kustomize_v%s_%s_%s.tar.gz", tool.Version, tool.Version, checksumOS, checksumArch)
+			}
+
+			if url == "" {
+				fmt.Printf("error: unsupported tool\n")
+				continue
+			}
+
+			fmt.Printf("URL: %s\n", url)
+
+			headResponse, err := http.Head(url)
+			if err != nil {
+				return err
+			}
+			defer headResponse.Body.Close()
+
+			if headResponse.StatusCode > 200 {
+				fmt.Printf("error: invalid status code %d\n", headResponse.StatusCode)
+				continue
+			}
+
+			contentLength, err := strconv.Atoi(headResponse.Header.Get("content-length"))
+			if err != nil {
+				return err
+			}
+
+			bar := pb.Full.Start(contentLength)
+
+			getResponse, err := http.Get(url)
+			if err != nil {
+				return err
+			}
+			defer getResponse.Body.Close()
+
+			barReader := bar.NewProxyReader(getResponse.Body)
+
+			fileBytes, err := io.ReadAll(barReader)
+			if err != nil {
+				return err
+			}
+
+			bar.Finish()
+
+			fileChecksumBytes := sha256.Sum256(fileBytes)
+			fileChecksum := hex.EncodeToString(fileChecksumBytes[:])
+
+			fmt.Printf("Checksum is %s\n", fileChecksum)
+
+			tool.Checksums[checksumOSAndArch] = fileChecksum
+		}
+	}
+
+	outputData, err := yaml.Marshal(kfdFile)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Output:\n%s\n", strings.ReplaceAll(string(outputData), "    ", "  "))
+
+	return nil
+}
