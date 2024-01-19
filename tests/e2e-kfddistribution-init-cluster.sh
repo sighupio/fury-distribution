a252cc395f0b4eaa6648f3fcde90abbc373a6225
Samuele Chiocca <samuele@nutellino.it>
2024-01-19 16:18:29 +0100

    Feat: introduce e2e tests using furyctl and KFDDistribution (#161)
    * feat: start implementing e2e using furyctl and KFDDistribution provider
    
    * feat: put correct kubeconfig on furyctl-1.yaml file
    
    * fix: lint problem on e2e script
    
    * feat: rename furyctl e2e config file, with a description in the name
    
    * feat: update drone e2e testing plugin to v2.1.0
    
    * feat: move furyctl test outside bats, to see full output during executions
    
    * feat: removing storageclass from e2e plugins, with kind there should already be a storageclass available
    
    * fix: furyctl path on e2e tests
    
    * fix: remove non-existent e2e bash script
    
    * feat: revert e2e-testing-drone-plugin to v2.0.0 and add -H flag to furyctl on e2e tests
    
    * bug: env is not working on kubeconfig parameter, moving to hardcoded value
    
    * feat: simplifying deploy using calico instead of cilium
    
    * feat: removing debug flag from furyctl
    
    * feat: do tests for each furyctl config
    
    * feat: moving schema-test on it's own step, renaming e2e in e2e-kfddistribution
    
    * feat: removing duplicated schema test step, already present in the non-tag pipelines
    
    * fix: removing common config for the distribution
    
    * feat: update furyctl, reduce footprint on some components since we have only 12Gb of ram on e2e tests
    
    * fix: calico tests
    
    * feat: add tests on migrate from tempo to none
    
    * feat: add kyverno migration
    
    * Apply suggestions from code review
    
    Co-authored-by: Ramiro Algozino <ramiro@sighup.io>
    
    * chore: from sighup.io to sighup.cc
    
    ---------
    
    Co-authored-by: Ramiro Algozino <ramiro@sighup.io>

---
+++ tests/e2e-kfddistribution-init-cluster.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bats
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# shellcheck disable=SC2154
+
+load ./helper
+
+
+@test "Calico Kube Controller is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=calico-kube-controllers -o json -n calico-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Calico Node is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=calico-node -o json -n calico-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Velero is Running" {
+    info
+    test() {
+        kubectl get pods -l k8s-app=velero -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Velero Node Agent is Running" {
+    info
+    test() {
+        kubectl get pods -l k8s-app=velero-node-agent -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Cert-manager Controller is Running" {
+    info
+    test() {
+        kubectl get pods -l app=cert-manager -o json -n cert-manager |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Nginx Ingress Controller is Running" {
+    info
+    test() {
+        kubectl get pods -l app=ingress-nginx -o json -n ingress-nginx |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Forecastle is Running" {
+    info
+    test() {
+        kubectl get pods -l app=forecastle -o json -n ingress-nginx |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Fluentd is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=fluentd -o json -n logging |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+
+@test "Grafana is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=grafana -o json -n monitoring |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Prometheus Operator is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=prometheus-operator -o json -n monitoring |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Kube State Metrics is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=kube-state-metrics -o json -n monitoring |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Node Exporter is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=node-exporter -o json -n monitoring |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "Prometheus is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=prometheus -o json -n monitoring | jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+
+    }
+    loop_it test 160 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
+@test "kube-proxy-metrics is Running" {
+    info
+    test() {
+        kubectl get pods -l k8s-app=kube-proxy-metrics -o json -n monitoring | jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
