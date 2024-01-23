86a87fafea1af8e768c095b5c7d1fcd7d7dcd853
Samuele Chiocca <samuele@sighup.io>
2024-01-23 08:48:19 +0100

    feat: add --skip-deps-download on subsequent tests (after init), since we are using the same distro version

--- tests/e2e-kfddistribution.sh
+++ tests/e2e-kfddistribution.sh
@@ -13,35 +13,35 @@ bats -t tests/e2e-kfddistribution-init-cluster.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the tempo migration to none"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-2-migrate-from-tempo-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-2-migrate-from-tempo-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-2-migrate-from-tempo-to-none.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the kyverno migration to none"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-3-migrate-from-kyverno-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-3-migrate-from-kyverno-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-3-migrate-from-kyverno-to-none.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the velero migration to none"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-4-migrate-from-velero-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-4-migrate-from-velero-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-4-migrate-from-velero-to-none.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the logging migration to none"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-5-migrate-from-loki-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-5-migrate-from-loki-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-5-migrate-from-loki-to-none.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the nginx migration to none"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-6-migrate-from-nginx-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-6-migrate-from-nginx-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-6-migrate-from-nginx-to-none.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the auth basic to sso migration"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-7-migrate-from-basicAuth-to-sso.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-7-migrate-from-basicAuth-to-sso.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-7-migrate-from-basicAuth-to-sso.sh
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with the auth basic to sso migration"
-/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-8-migrate-from-sso-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force
+/tmp/furyctl create cluster --config tests/e2e/kfddistribution/furyctl-8-migrate-from-sso-to-none.yaml --outdir "$PWD" -H --distro-location ./ --force --skip-deps-download
 bats -t tests/e2e-kfddistribution-8-migrate-from-sso-to-none.sh
