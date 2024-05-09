16cb8ebc328002dfe86d81f9960550d0406c0ee1
Alessio Pragliola <83355398+Al-Pragliola@users.noreply.github.com>
2024-05-09 16:16:31 +0200

    feat: prepare release 1.29.1 (#218)
    * feat: prepare release 1.29.1
    
    * chore: update furyctl version in drone
    
    * fix: removed wrong info about new module version

--- tests/e2e-kfddistribution-upgrades.sh
+++ tests/e2e-kfddistribution-upgrades.sh
@@ -7,8 +7,8 @@ set -e
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl for the initial setup"
-/tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.28.0.yaml --outdir "$PWD" --disable-analytics
+/tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.0.yaml --outdir "$PWD" --disable-analytics
 
 echo "----------------------------------------------------------------------------"
 echo "Executing upgrade to the next version"
-/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.0.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.1.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
