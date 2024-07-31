07898a34eb3c1669fb07d523e959f5b4467ad314
Samuele Chiocca <samuele@sighup.io>
2024-07-31 17:05:21 +0200

    feat: prepare to test 1.29.2 rcs

--- tests/e2e-kfddistribution-upgrades.sh
+++ tests/e2e-kfddistribution-upgrades.sh
@@ -10,5 +10,9 @@ echo "Executing furyctl for the initial setup"
 /tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.0.yaml --outdir "$PWD" --disable-analytics
 
 echo "----------------------------------------------------------------------------"
+echo "Executing upgrade to an intermediate version"
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.1.yaml --outdir "$PWD" --force upgrades --disable-analytics
+
+echo "----------------------------------------------------------------------------"
 echo "Executing upgrade to the next version"
-/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.1.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.2.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
