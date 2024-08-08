37198f9c891c80dfe25497ffab0762a14e8c11f2
Samuele Chiocca <samuele@sighup.io>
2024-08-08 11:04:25 +0200

    feat: update and align tests from 1.29.2

--- tests/e2e-kfddistribution-upgrades.sh
+++ tests/e2e-kfddistribution-upgrades.sh
@@ -10,5 +10,8 @@ echo "Executing furyctl for the initial setup"
 /tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.28.0.yaml --outdir "$PWD" --disable-analytics
 
 echo "----------------------------------------------------------------------------"
+echo "Executing upgrade to an intermediate version"
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.28.1.yaml --outdir "$PWD" --force upgrades --disable-analytics
+echo "----------------------------------------------------------------------------"
 echo "Executing upgrade to the next version"
-/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.28.1.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.28.2.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
