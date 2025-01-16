6853060100589777816b90af92a105b728bd1e39
Samuele Chiocca <samuele@nutellino.it>
2025-01-16 12:48:44 +0100

    feat: release v1.30.1 with backport features from v1.31.0 (#330)
    * feat: backport v1.31.0 to v1.30.1 without kapp feature
    
    * docs: add all docs from 1.31.0, 1.30.1, 1.29.6 and 1.28.6
    
    * docs: apply suggestions from review
    
    * chore: regenerate docs and schemas
    
    * docs: update compatibility matrix
    
    * chore: removing unneeded comment

--- tests/e2e-kfddistribution-upgrades.sh
+++ tests/e2e-kfddistribution-upgrades.sh
@@ -10,8 +10,7 @@ echo "Executing furyctl for the initial setup 1.29.4"
 /tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.29.4.yaml --outdir "$PWD" --disable-analytics
 
 echo "----------------------------------------------------------------------------"
-echo "Executing upgrade to 1.30.0"
+echo "Executing upgrade to 1.30.1"
 # we set the switch date for Loki to "tomorrow". Notice that `-d flag` does not work on Darwin, you need to use `-v +1d` instead.
-# this is needed only when upgrading from 1.29.4 to 1.30.0 (and equivalent versions)
-yq -i ".spec.distribution.modules.logging.loki.tsdbStartDate=\"$(date -I -d '+1 day')\"" tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.30.0.yaml
-/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.30.0.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
+yq -i ".spec.distribution.modules.logging.loki.tsdbStartDate=\"$(date -I -d '+1 day')\"" tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.30.1.yaml
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.30.1.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
