25c83f52cee745776058ff7c6beddf2ac97cc554
Filo01 <github@filippo.pw>
2025-03-06 12:48:43 +0100

    fix: lint

--- tests/e2e/ekscluster/e2e-ekscluster.sh
+++ tests/e2e/ekscluster/e2e-ekscluster.sh
@@ -82,20 +82,22 @@ bats -t tests/e2e/ekscluster/e2e-ekscluster-7-migrate-from-basicAuth-to-sso.sh
 
 #Temporary fix to delete pomerium dns 
 hosted_zone_id=$(aws route53 list-hosted-zones \
-  --output text \
-  --query "HostedZones[?Name==$CLUSTER_NAME.e2e.ci.sighup.cc.)].Id")
+--output text \
+--query "HostedZones[?Name==$CLUSTER_NAME.e2e.ci.sighup.cc.)].Id")
+
 aws route53 list-resource-record-sets \
-  --hosted-zone-id "$hosted_zone_id" | \
+--hosted-zone-id "$hosted_zone_id" | \
 jq -c '.ResourceRecordSets[]' | \
 while read -r resourcerecordset; do
-    read -r name type <<<$(echo $(jq -r '.Name,.Type' <<<"$resourcerecordset"))
-  if [ "$type" != "NS" -a "$type" != "SOA" ]; then
+  type=$(echo "$resourcerecordset" | jq -r '.Type')
+  
+  if [ "$type" != "NS" ] && [ "$type" != "SOA" ]; then
     aws route53 change-resource-record-sets \
-      --hosted-zone-id "$hosted_zone_id" \
-      --change-batch '{"Changes":[{"Action":"DELETE","ResourceRecordSet":
-          '"$resourcerecordset"'
-        }]}' \
-      --output text --query 'ChangeInfo.Id'
+    --hosted-zone-id "$hosted_zone_id" \
+    --change-batch '{"Changes":[{"Action":"DELETE","ResourceRecordSet":
+'"$resourcerecordset"'
+}]}' \
+    --output text --query 'ChangeInfo.Id'
   fi
 done
 
